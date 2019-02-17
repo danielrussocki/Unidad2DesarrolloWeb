25e53bed4b9f0c551e600c3802e3338d7341a54f
danielrussocki <46513699+danielrussocki@users.noreply.github.com>
2019-02-17 18:35:26 -0500

    Login funcionando
    -Conectado a la bd smoothoperators
    -Validada existencia en bd
    -Valida contraseña incorrecta
    -Valida si los campos están vacíos
    -Valida acceso

--- backend/includes/_funciones.php
+++ backend/includes/_funciones.php
@@ -1,7 +1,7 @@
 <?php
-require_once("_db.php");
+require_once '_db.php';
 switch ($_POST["accion"]) {
-	case 'login':
+	case "login":
 		login();
 		break;
 	
@@ -10,13 +10,54 @@ switch ($_POST["accion"]) {
 		break;
 }
 function login(){
-	echo "Tu usuario es: ".$_POST["usuario"]." y tu password es: ".$_POST["password"];
+	//echo "Tu usuario es: ".$_POST["usuario"]." y tu password es: ".$_POST["password"];
 	//Conectar con la base de datos
+	global $mysqli;
+	$usu = $_POST["usuario"];
+	$pass = $_POST["password"];
+	$num = 0;
 	//Si usuario y contraseña están vacíos que imprima 3
+	if ($usu==''||$pass=='') {
+		$num = 3;
+	} else {
+		$query = "SELECT * FROM usuarios WHERE correo_usr = '$usu'";
+		$result = $mysqli->query($query);
+		if ($result->num_rows == 0) {
+			$num = 2;
+		} else {
+			$query2 = "SELECT * FROM usuarios WHERE correo_usr = '$usu' AND pswd_usr = '$pass'";
+			$result2 = $mysqli->query($query2);
+			if ($result2->num_rows > 0) {
+				$num = 1;
+			} elseif ($result2->num_rows == 0) {
+				$num = 0;
+			}
+		}
+	}
+	imprimir($num);
 	//Consultar a la base de datos que el usuario exista
 	//Si el usuario existe, consultar que el password sea correcto
 	//Si el password es correcto, imprimir 1
 	//Si el password no es correcto, imprimir 0
-	//Si el usuario no existe, imprimir 0
+	//Si el usuario no existe, imprimir 2
+}
+function imprimir($n){
+	switch ($n) {
+		case 0:
+			echo "Contraseña incorrecta - 0";
+			break;
+		case 1:
+			echo "Acceso permitido - 1";
+			break;
+		case 2:
+			echo "El usuario no existe - 2";
+			break;
+		case 3:
+			echo "Favor de llenar los campos - 3";
+			break;
+		default:
+			# code...
+			break;
+	}
 }
 ?>
